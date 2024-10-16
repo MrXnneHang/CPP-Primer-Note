6fac717389f0c03034666dc5899a346d19913454
MrXnneHang <XnneHang@gmail.com>
2024-10-16 09:01:28 +0800

    读取不定多组书籍ISBN以及销售数据，打印出来

--- 1-开始/1.20.cpp
+++ 1-开始/1.20.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include"Sales_item.h"
 
+// 读取不定多组数据,然后把每组数据打印出来。
+
 
 int main()
 {
-    Sales_item item1, item2;
-    std::cin >> item1 >> item2;
-    std::cout << item1 + item2 << std::endl;
+    Sales_item item;
+    while (std::cin>>item)
+    {
+        std::cout << item << std::endl;
+    }
     return 0;
 }
